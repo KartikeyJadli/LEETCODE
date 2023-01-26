c64a350899dbeb6c2b6cf4aff8fba542e864f45d
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2023-01-26 15:12:28 +0530

    Time: 4 ms (77.38%), Space: 9 MB (93.50%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int diff = arr[1] - arr[0]; 
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i+1] - arr[i] != diff){
+                cout << arr[i+1] - arr[i] << " ";
+                return false;
+            }
+
+            if(arr[arr.size() - 1] - arr[arr.size() - 2] != diff){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
