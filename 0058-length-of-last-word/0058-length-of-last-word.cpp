dd91e0bef5cda659b57d4e98ba9c98ce2567940c
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2023-02-10 11:05:06 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (11.56%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        string str = "";
+        int count = 0;
+        int n = s.length() - 1;
+        
+        while(n >= 0 && s[n] == ' '){
+            n--;
+        }
+
+        while(n >= 0 && s[n] != ' '){
+            count++;
+            n--;
+        }
+
+        return count;
+    }
+};
