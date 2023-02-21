96a3a5f95b79f3314233961298436e3e7dd067af
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2023-02-21 11:25:31 +0530

    Time: 336 ms (45.71%), Space: 71.5 MB (46.72%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        nth_element(points.begin(),points.begin() + k,points.end(), [](vector<int>& a, vector<int>& b) {
-            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
-        });
+        // nth_element(points.begin(),points.begin() + k,points.end(), [](vector<int>& a, vector<int>& b) {
+        //     return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
+        // });
 
-        return vector<vector<int>>(points.begin(),points.begin() + k);
+        // return vector<vector<int>>(points.begin(),points.begin() + k);
+        
+        vector<vector<int>> v;
+        map<vector<int>,int> mp;
+        vector<pair<int,pair<int,int>>> v1;
+
+        for(auto x: points){
+            int diff = x[0]*x[0] + x[1]*x[1];
+            cout << diff << endl; 
+            v1.push_back({diff,{x[0],x[1]}});
+        }
+
+        sort(v1.begin(),v1.end());
+
+        for(int i=0;i<k;i++){
+            v.push_back({v1[i].second.first,v1[i].second.second});
+        }
+
+        return v;
     }
 };
