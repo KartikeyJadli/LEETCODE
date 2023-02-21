b04d339783fb4e9fb47bec4fcf9ff980721d25ba
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2023-02-21 18:57:36 +0530

    Time: 249 ms (93.80%), Space: 119.5 MB (19.87%) - LeetHub

---
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    typedef pair<int, pair<int, int>> ppii;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>> result;
+        priority_queue<ppii,vector<ppii>, greater<ppii>> pq;
+
+        for(int i=0;i<nums1.size();i++) {
+            pq.push({nums1[i]+nums2[0], {i,0}});
+        }
+
+        while(!pq.empty() && k--){
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
+            pq.pop();
+            
+            result.push_back({nums1[i],nums2[j]});
+            
+            if(j + 1 == nums2.size()){
+                continue;
+            }
+
+            pq.push({nums1[i] + nums2[j+1],{i,j+1}});
+        }
+
+        return result;     
+    }
+};
