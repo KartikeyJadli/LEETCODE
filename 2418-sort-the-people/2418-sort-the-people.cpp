4e31b327d45efa32a18f40cfb3c9a303b9b25ac9
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2023-03-26 20:36:29 +0530

    Time: 46 ms (32.25%), Space: 22.9 MB (67.73%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        priority_queue<pair<int,string>> pq;
+        vector<string> v;
+        int n = names.size();
+        for(int i=0;i<n;i++){
+            pq.push({heights[i],names[i]});
+        }
+
+        while(!pq.empty()){
+            v.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return v;
+    }
+};
