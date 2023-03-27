ce1fd2ea44bf6714e18c3991b1a887beef66a29c
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2023-03-27 06:52:21 +0530

    Time: 7 ms (90.29%), Space: 10.2 MB (7.26%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size(); 
+
+        vector<vector<int>> v(m, vector<int>(n, grid[0][0]));
+        for (int i=1;i<m;i++){
+            v[i][0] = v[i - 1][0] + grid[i][0];
+        }
+
+        for (int j=1;j<n;j++){
+            v[0][j] = v[0][j - 1] + grid[0][j];
+        }
+
+        for (int i=1;i<m;i++){
+            for (int j=1;j<n;j++){
+                v[i][j]  = min(v[i - 1][j], v[i][j - 1]) + grid[i][j];
+            }
+        }
+        
+        return v[m - 1][n - 1];
+    }
+};
