dcb58b63e68db29e770dc130bfb2df77227f7de2
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2023-03-31 19:00:57 +0530

    Time: 7 ms (54.56%), Space: 9.8 MB (32.19%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> v;
-        int j = n;
-
-        for(int i=0;i<n;i++){
+        int i = 0,j = n;
+        while(i < n){
             v.push_back(nums[i]);
-            while(j < 2*n){
-                v.push_back(nums[j]);
-                j++;
-                break;
-            }
+            v.push_back(nums[j]);
+            j++;
+            i++;
         }
 
         return v;
