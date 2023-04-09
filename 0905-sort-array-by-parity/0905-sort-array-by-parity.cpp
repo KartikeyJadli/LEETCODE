cae4e5aab3002fd2278082cd0668e2dd4ec02807
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2023-04-09 18:25:01 +0530

    Time: 12 ms (56.05%), Space: 16.2 MB (67.65%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i = 0,j = nums.size() - 1;
+        while(i <= j){
+            if(nums[i] % 2 == 0){
+                i++;
+            }
+
+            else if(nums[i] % 2 != 0){
+                if(nums[j] % 2 == 0){
+                    swap(nums[i],nums[j]);
+                    j--;
+                }
+
+                else{
+                    j--;
+                }
+            }
+        }
+
+        return nums;
+    }
+};
