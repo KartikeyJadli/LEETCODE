4e6d68deebee8542ca8784abf9d4feec05704958
Kartikey Jadli <96066261+KartikeyJadli@users.noreply.github.com>
2023-05-08 20:44:37 +0530

    Time: 27 ms (56.73%), Space: 31.1 MB (63.49%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0,right = 0;
+        
+        for(int i=0;i<n;i++){
+            right += nums[i];
+        }
+        
+        for(int i=0;i<n;i++){
+            right -= nums[i];
+            if(left == right){
+                return i;
+            }
+            
+            left += nums[i];
+        }
+        
+        return -1;
+    }
+};
